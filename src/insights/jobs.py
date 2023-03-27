5f75868c861859264d8a3c564818351bbfbac195
Milena Suzumura <milena.suzumura11@gmail.com>
2023-03-27 19:50:47 -0300

    retorna a quantidade total de jobs

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -16,21 +16,13 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
+    people = read(path)
+    test = []
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    for person in people:
+        test.append(person)
+        print(test)
+    return len(people)
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
