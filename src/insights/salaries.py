93118a7810e0a1e07df5bb6a6f1717d22737fe6d
Milena Suzumura <milena.suzumura11@gmail.com>
2023-03-27 20:31:56 -0300

    requisito 4

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,22 +1,20 @@
 from typing import Union, List, Dict
+from src.insights.jobs import read
 
 
 def get_max_salary(path: str) -> int:
-    """Get the maximum salary of all jobs
+    people = read(path)
+    maxSalary = 0
 
-    Must call `read`
+    for person in people:
+        try:
+            max_salary = int(person["max_salary"])
+            if max_salary > maxSalary:
+                maxSalary = int(person["max_salary"])
+        except ValueError:
+            pass
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    return maxSalary
 
 
 def get_min_salary(path: str) -> int:
